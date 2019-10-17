0ec690f83eb864524c149fd2f07044287d8fdd51
TamO <qooga.work+github@gmail.com>
2019-10-17 16:15:14 +0900

    コミット忘れていたなんてことはない

---
+++ dist/jquery.konami.js
@@ -0,0 +1,95 @@
+/*!
+ * KONAMI COMMAND jQuery Plugin - A jQuery Framework for implementing Konami commands
+ * Version: 1.0.0
+ * Url: https://qooga.jb-jk.net/item/js/konami/ https://qooga.jb-jk.net/konami-command/
+ * Author: Tam 0
+ * Author url: https://qooga.jb-jk.net/
+ * License: The MIT License (MIT)
+ * License url: https://qooga.jb-jk.net/terms/
+ * Date: 2019-08-29
+ * Git: https://github.com/qooga-work/konami-command-jquery
+ * Copyright (C) 2019 qooga.jb-jk.net - A project by Tam O
+ */
+(function ($) {
+  jQuery.fn.konami = function(options) {
+    // 厳格モード
+    'use strict';
+
+    // オプションで変動するモノ
+    var defaults = {
+      // 比較対象コマンドデータ
+      // 38:上 40:下 37:左 39:右 66:B 65:A
+      setCommand: [38,38,40,40,37,39,37,39,66,65],
+      // 起動時
+      onFunction: function(){alert("Start konami-command!");},
+      // 停止時
+      offFunction: function(){alert("End konami-command!");},
+      // 保存するCookie名
+      cookieName: 'konamiCMD',
+    };
+    var setting = jQuery.extend(defaults, options);
+
+    // 入力されたキーを保存する
+    var inputKey = [];
+
+    // 文字数
+    var keyLength = setting.setCommand.length;
+
+    // cookie配列
+    var readCookie = [];
+
+    // コマンド発動しているか　1:有効 0:無効
+    var nowType = 0;
+
+    // 画面上のキー入力イベントリスナ
+    $(window).keyup(function(e) {
+
+      // キー入力を配列に追加
+      inputKey.push(e.keyCode);
+
+      // 少なければ終了
+      if(inputKey.length < keyLength){
+        return false;
+      }
+      // 多ければ先入れを消す
+      if(inputKey.length > keyLength){
+        inputKey.splice(0, inputKey.length - keyLength);
+      }
+
+      // キー入力が保存された配列とコマンドを比較
+      if (inputKey.toString().indexOf(setting.setCommand) >= 0) {
+        // コマンド成功処理
+
+        // cookieデータを1つずつに分ける
+        var tc = document.cookie.split(';');
+        tc.forEach(function(value) { 
+          //cookie名と値に分ける
+          var ts = value.split('=');
+          readCookie[ts[0]] = ts[1];
+        });
+//      $.each(readCookie, function(t_key, t_value) {
+//        console.log(t_key+'='+t_value);
+//      })
+
+        nowType = 0;
+        if(!readCookie[setting.cookieName]){
+          nowType = 1;
+        }else if(readCookie[setting.cookieName] == 0){
+          nowType = 1;
+        }
+        // クッキーに保存
+        document.cookie = setting.cookieName+'='+nowType;
+
+        if(nowType == 1){ // ON
+          setting.onFunction();
+        }else{ // OFF
+          setting.offFunction();
+        }
+
+        //　キー入力を初期化
+        inputKey = [];
+      }
+    });
+
+  };
+})(jQuery);
---
+++ konami.php
@@ -0,0 +1,114 @@
+<?php
+// Cookie消す処理
+if(isset($_REQUEST['delete_cookie'])){
+    setcookie('konamiCMD', '', time() - 1800);
+    header('Location: ./konami.php', true , 301);
+}
+?>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>コナミコマンドテスト</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="./favicon.ico" />
+<script src="./jquery-3.4.1.min.js" type="text/javascript"></script>
+<script src="./dist/jquery.konami.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(function(){
+    var mySet = [];
+    mySet.wordOn = 'ON！！';
+    mySet.wordOff = 'OFF！！';
+    mySet.checkCookie = function(flag=0){
+        var tc = document.cookie.split(';');
+        var readCookie = [];
+        tc.forEach(function(value) { 
+          //cookie名と値に分ける
+          var ts = value.split('=');
+          readCookie[ts[0]] = ts[1];
+        });
+        if(!readCookie.konamiCMD){
+            return mySet.end(flag);
+        }else if(readCookie.konamiCMD == 0){
+            return mySet.end(flag);
+        }else{
+            return mySet.start(flag);
+        }
+    };
+
+    // 起動時オプション例
+    mySet.start = function(flag=0){
+        $('.set_now').text(mySet.wordOn);
+        $('body').addClass('konami_on');
+        if(flag==0)
+          alert("コナミコマンド発動！");
+    };
+    // 停止時オプション例
+    mySet.end = function(flag=0){
+        $('.set_now').text(mySet.wordOff);
+        $('body').removeClass('konami_on');
+        if(flag==0)
+          alert("コナミコマンド停止！");
+    };
+
+
+    $('body').konami({
+      onFunction: function(){mySet.start()},
+      offFunction: function(){mySet.end()},
+    });
+    mySet.checkCookie(1);
+});
+</script>
+<link href="./style.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>コナミコマンドセット例</h1>
+
+<p>Cookie内容の表示とCookie削除以外は、jQueryだけで処理しています。</p>
+
+
+<h2>現在の状態</h2>
+<span class="set_now"></span>
+
+
+<h2>Cookieの内容</h2>
+<div class="cookie_check">
+<pre><?php
+if(isset($_COOKIE)){
+  print_r($_COOKIE);
+}else{
+  echo 'cookieナシ';
+}
+?></pre>
+</div>
+
+<form method="post" action="./konami.php">
+<input type="submit" name="delete_cookie" class="button" value="Cookieを消す">
+</form>
+
+
+<h2>入力テストのための無意味なフォーム</h2>
+<form method="post" action="./konami.php">
+<input type="text" value="" placeholder="意味はないINPUT" ><br />
+<textarea cols="50" rows="5" placeholder='KONAMIコマンドは『↑↑↓↓←→←→BA』
+form内で打ち込んだ場合も有効になります。'></textarea>
+</form>
+
+<a href="./">説明ページへ戻る</a>
+
+<div class="ninja_onebutton">
+<script type="text/javascript">
+//<![CDATA[
+(function(d){
+if(typeof(window.NINJA_CO_JP_ONETAG_BUTTON_eb03a8ac3330de6bdd3765b02ec70a7e)=='undefined'){
+    document.write("<sc"+"ript type='text\/javascript' src='\/\/omt.shinobi.jp\/b\/eb03a8ac3330de6bdd3765b02ec70a7e'><\/sc"+"ript>");
+}else{
+    window.NINJA_CO_JP_ONETAG_BUTTON_eb03a8ac3330de6bdd3765b02ec70a7e.ONETAGButton_Load();}
+})(document);
+//]]>
+</script><span class="ninja_onebutton_hidden" style="display:none;"></span><span style="display:none;" class="ninja_onebutton_hidden"></span>
+</div>
+
+<address><a href="https://qooga.jb-jk.net/" target="_blank">©クーガキカク</a></address>
+</body>
+</html>
